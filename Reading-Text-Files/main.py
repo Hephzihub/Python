77f2bc65964488be375ab9b12f3bf2bfd674c0af
Oluwasegun Adedeji <64655766+Hephzihub@users.noreply.github.com>
2022-05-25 19:13:55 +0100

    Update main.py
    This code converts a text file to a string and gives the frequency of each word

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -8,27 +8,34 @@ def read_file_content(filename):
 
     # Reading the text file
     text_file = open(filename)
+    # Reads the string
     text_data = text_file.read()
+    #Coverts all uppercase to lowercase
     text_data = text_data.lower()
 
     # Remove punctuations using RegEX
     text_data = re.sub(r'[^\w\s]', '', text_data)
-
+    
+    # Returns the text without double spaces and new line
     return text_data.replace("  \n", " ")
 
 def count_words():
+    #The read_file_content() is invoked with a parameters and the returned value is saved
     text = read_file_content("./story.txt")
 
-    # [assignment] Add your code here
-
+    #The string is coverted into a list
     text_list = text.split(" ")
 
+    # A new list is created
     text_new_list = []
-
+    
+    #This control flow puts the strings in old list to new list with reoccurrence
     for i in text_list:
 
+        #Checks if word already exist
         if i not in text_new_list:
             
+            #adds word
             text_new_list.append(i)
     for i in range(0, len(text_new_list)):
 
